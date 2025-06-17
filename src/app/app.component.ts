9f40f51620df1544e5f323abeedf2da67a47edab
elvis michael paulino <epaulino@rauroszm.com>
2025-06-17 17:40:17 +0200

    se creo el componente de formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,63 @@
 import { Component, signal, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SintasisTemplateComponent } from "./sintasis-template/sintasis-template.component";
-import { EventosComponent } from "./eventos/eventos.component";
-import { EnlacePropiedadesComponent } from "./Enlace-Propiedades/Enlace-Propiedades.component";
+import { SintasisTemplateComponent } from './sintasis-template/sintasis-template.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { EnlacePropiedadesComponent } from './Enlace-Propiedades/Enlace-Propiedades.component';
 import { DadoComponent } from './dado/dado.component';
 import { CronometroComponent } from './Cronometro/Cronometro.component';
 import { SelectorNumericoComponent } from './selector-numerico/selector-numerico.component';
 import { SelectorNumericoTwoComponent } from './selector-numerico-two/selector-numerico-two.component';
+import { FormsModule } from '@angular/forms'; 
+import { CommonModule } from '@angular/common';
+import { FormularioPlantillasComponent } from "./formulario-plantillas/formulario-plantillas.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
     SintasisTemplateComponent,
     EventosComponent,
     EnlacePropiedadesComponent,
     DadoComponent,
     CronometroComponent,
     SelectorNumericoComponent,
-    SelectorNumericoTwoComponent
+    SelectorNumericoTwoComponent,
+    FormsModule,
+    CommonModule,
+    FormularioPlantillasComponent
 ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
 
-  mensaje = signal('')
+  // nombre  = signal('');
+  // apellido = signal('')
+
+  nombre = '';
+  apellido= '';
+
+  constructor(){
+      // this.nombre.set('Maria')
+  }
 
 
-actualizar(t: number) {
-  this.mensaje.update(x => t + ' (se actualiza cada 10 segundos)');
-}
+
+
+  // articulos = signal<any[]>([
+  //   {codigo: 1, description : 'papas', precio: 10.55},
+  //   {codigo: 2, description : 'manzanas', precio: 12.10},
+  //   {codigo: 3, description : 'melon', precio: 52.30},
+  //   {codigo: 4, description : 'cebollas', precio: 17},
+  //   {codigo: 5, description : 'calazaba', precio: 20},
+
+  // ]);
+
+  mensaje = signal('');
+
+  actualizar(t: number) {
+    this.mensaje.update((x) => t + ' (se actualiza cada 10 segundos)');
+  }
 
   // title = 'angular-19-ya';
   // nombre = 'Rodriguez Pablo';
@@ -41,65 +67,57 @@ actualizar(t: number) {
   // activo = true;
   // resultado= '';
 
+  //  valor1: number = this.retornarAleatorio();
+  //  valor2: number = this.retornarAleatorio();
+  //  valor3: number = this.retornarAleatorio();
 
-  
-//  valor1: number = this.retornarAleatorio();
-//  valor2: number = this.retornarAleatorio();
-//  valor3: number = this.retornarAleatorio();
+  //   sitioWeb= 'https://www.google.com';
 
-//   sitioWeb= 'https://www.google.com'; 
+  //  esActivo(){
 
+  //   if(this.activo){
+  //     return 'Trabajador activo';
+  //   }
+  //   else{
+  //     return 'Trabajador inactivo';
+  //   }
+  //  }
 
-//  esActivo(){
+  //  ultimos3sueldos(){
+  //   let suma = 0;
 
-//   if(this.activo){
-//     return 'Trabajador activo';
-//   }
-//   else{
-//     return 'Trabajador inactivo'; 
-//   }
-//  }
+  //   for(let x=0; x>this.sueldos.length; x++){
+  //     suma += this.sueldos[x];
+  //   }
+  //  }
 
-//  ultimos3sueldos(){
-//   let suma = 0;
+  //  retornarAleatorio(){
+  //   return Math.floor(Math.random() * 6 ) + 1;
+  // }
 
-//   for(let x=0; x>this.sueldos.length; x++){
-//     suma += this.sueldos[x];
-//   }
-//  }
+  // tirar(){
 
-//  retornarAleatorio(){
-//   return Math.floor(Math.random() * 6 ) + 1;
-// }
+  //   this.valor1 = this.retornarAleatorio();
+  //   this.valor2 = this.retornarAleatorio();
+  //   this.valor3 = this.retornarAleatorio();
 
-// tirar(){
-
-//   this.valor1 = this.retornarAleatorio();
-//   this.valor2 = this.retornarAleatorio();
-//   this.valor3 = this.retornarAleatorio();
-
-//   if(this.valor1 === this.valor2 && this.valor1 === this.valor3){
-//     this.
-//     resultado = 'Gano'
-//   }
-//   else{
-//     this.resultado = 'Perdio';
-//   }
-// }
+  //   if(this.valor1 === this.valor2 && this.valor1 === this.valor3){
+  //     this.
+  //     resultado = 'Gano'
+  //   }
+  //   else{
+  //     this.resultado = 'Perdio';
+  //   }
+  // }
 
   @ViewChild('selector1') selector1!: SelectorNumericoTwoComponent;
   @ViewChild('selector2') selector2!: SelectorNumericoTwoComponent;
 
-  fijarSelector1(valor:number) {
+  fijarSelector1(valor: number) {
     this.selector1.fijar(valor);
   }
 
-  fijarSelector2(valor:number) {
+  fijarSelector2(valor: number) {
     this.selector2.fijar(valor);
   }
-
-
 }
-
-
-
