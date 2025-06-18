744df79b0213eed951aad9cdbe8776a51db41317
elvis michael paulino <epaulino@rauroszm.com>
2025-06-18 09:57:06 +0200

    Se creo la parte 3 de formulario

--- src/app/formulario-reactivos-three/formulario-reactivos-three.component.ts
+++ src/app/formulario-reactivos-three/formulario-reactivos-three.component.ts
@@ -1,13 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-reactivos-three',
   standalone: true,
-  imports:[],
+  imports:[ReactiveFormsModule],
   templateUrl: './formulario-reactivos-three.component.html',
   styleUrls: ['./formulario-reactivos-three.component.css']
 })
 export class FormularioReactivosThreeComponent implements OnInit {
+
+ conversion= ''
+ potenciaNumero = ''
+ cantidad = ''
+
+ formularioConversion = new FormGroup({
+  numeroDecimal: new FormControl(''),
+  base: new FormControl(''),
+  potencia: new FormControl(''),
+  largo: new FormControl(true)  
+ });
+
+ submit(){
+
+  if(this.formularioConversion.value.numeroDecimal){
+    debugger
+    if(this.formularioConversion.value.base == 'Hexadecimal'){
+      this.conversion =  parseInt(this.formularioConversion.value.numeroDecimal).toString(16);
+    }
+
+    if(this.formularioConversion.value.base ==  'octal'){
+      this.conversion =  parseInt(this.formularioConversion.value.numeroDecimal).toString(8);      
+    }
+
+    if(this.formularioConversion.value.numeroDecimal && this.formularioConversion.value.potencia){
+      this.potenciaNumero = Math.pow(parseInt(this.formularioConversion.value.numeroDecimal), parseInt(this.formularioConversion.value.potencia)).toString();
+    }
+
+    if(this.formularioConversion.value.largo){
+      this.cantidad =  this.formularioConversion.value.numeroDecimal.length.toString();
+    }
+
+
+
+
+
+
+
+  }
+ }
+
+
 
   constructor() { }
 
